ee9e40f78f8fa181115f539e686186bd09672283
Alexanderstaehle <48824183+Alexanderstaehle@users.noreply.github.com>
2020-11-26 09:54:07 +0100

    twitter stream initial

---
+++ twitter-scraper/README.md
@@ -0,0 +1,4 @@
+This is a WebScraper/Stream which takes Tweets live from a certain profile and sends them to an API endpoint which can be specified.
+For this to work you need a twitter developer account with the corresponding API keys and tokens.
+
+Don't forget to run `pip install -r requirements.txt` before launching.
---
+++ twitter-scraper/stream.py
@@ -0,0 +1,68 @@
+import tweepy
+
+from textblob import TextBlob
+
+from ssh.keys import API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+import json
+from pprint import pprint
+from pip._vendor import requests
+
+
+class StreamListener(tweepy.StreamListener):
+
+    def on_data(self, raw_data):
+        data = json.loads(raw_data)
+        if 'delete' in data:
+            print('deleted tweet')
+        else:
+            self.process_data(data)
+
+    def process_data(self, data):
+        pprint(data)
+        tweet = data['text']
+        text = tweet.replace('RT', '')
+        if text.startswith(' @'):
+            position = text.index(':')
+            text = text[position+2:]
+
+        if text.startswith('@'):
+            position = text.index(' ')
+            text = text[position+2:]
+
+        analysis = TextBlob(text)
+        tweet_polarity = analysis.polarity
+        put_data = {
+            "tweetId": data['id'],
+            "owner": data['user']['screen_name'],
+            "text": tweet
+        }
+        print(tweet_polarity)
+        print(put_data)
+        response = requests.put(
+            'https://stockalizer.azurewebsites.net/api/tweets?code=lZjlUl6QSaCXDJJANyhM8xAMtQUk6i1B90qzliaxKmNdLywWxfzUWw==', data=json.dumps(put_data))
+        print(response)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+
+
+class Stream():
+    def __init__(self, auth, listener):
+        self.stream = tweepy.Stream(auth=auth, listener=listener)
+
+    def start(self):
+        # das ist meine id
+        self.stream.filter(follow=["2418840943"], is_async=True)
+
+
+if __name__ == "__main__":
+    listener = StreamListener()
+
+    auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    stream = Stream(auth, listener)
+    try:
+        stream.start()
+    except Exception as e:
+        print(e)
