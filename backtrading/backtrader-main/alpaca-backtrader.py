f45db74dd00d07fe0ee8b74e25c9591a5fab0497
Alex <al851sta@htwg-konstanz.de>
2020-12-30 20:43:07 +0100

    paper trading setup

--- backtrading/backtrader-main/alpaca-backtrader.py
+++ backtrading/backtrader-main/alpaca-backtrader.py
@@ -6,87 +6,209 @@ from datetime import datetime
 import alpaca_backtrader_api
 from ssh.keys import API_KEY, API_SECRET_KEY
 
-from SentimentStrategy import SentimentStrategy
+ticker = 'AMZN'
+is_paper = True
+is_backtesting = True
+
+
+class SentimentStrategy(bt.Strategy):
+    params = (('period', 10), ('low_bound', 0.1), ('high_bound', 0.9))
+
+    def __init__(self):
+        self.live_bars = False
+        # self.tweet_sentiment = self.datas[2].sentiment_score
+        self.date = self.data.datetime
+        self.order = None
+
+    def notify_data(self, data, status, *args, **kwargs):
+        # super().notify_data(data, status, *args, **kwargs)
+        print('*' * 5, 'DATA NOTIF:', data._getstatusname(status), *args)
+        if data._getstatusname(status) == "LIVE":
+            self.live_bars = True
+
+    def next(self):
+        if not self.live_bars and not is_backtesting:
+            # skip if no live data yet available and we are not backtesting
+            return
+        """
+        if datetime.time(self.datas[0].datetime) < datetime.time(9, 45) or datetime.time(self.datas[0].datetime) > datetime.time(16, 0):
+            # don't operate until the market has been running 15 minutes or is closed again
+            return  #
+        """
+        # Check for open orders
+        if self.order:
+            return
+        self.news_sentiment = self.datas[1].sentiment_score
+        self.stock_price = self.datas[0].close
+        sentiment = self.news_sentiment
+        if sentiment > self.params.high_bound and not self.position:
+            self.log(f'Sentiment Value: {self.news_sentiment[0]:.2f}')
+            # We are not in the market, we will open a trade
+            self.log(f'***BUY CREATE {self.stock_price[0]:.2f}')
+            # Keep track of the created order to avoid a 2nd order
+            self.order = self.buy()
+        elif sentiment < self.params.low_bound and not self.position:
+            self.log(f'Sentiment Value: {self.news_sentiment[0]:.2f}')
+            # We are not in the market, we will open a trade
+            self.log(f'***SELL CREATE {self.stock_price[0]:.2f}')
+            # Keep track of the created order to avoid a 2nd order
+            self.order = self.sell()
+        else:
+            if self.position:
+                # We are in the market, we will close the existing trade
+                self.log(f'Sentiment Value: {self.news_sentiment[0]:.2f}')
+                self.log(f'CLOSE CREATE {self.stock_price[0]:.2f}')
+                self.order = self.close()
+
+    def notify_order(self, order):  # helper class for logging
+        if order.status in [order.Submitted, order.Accepted]:
+            # Existing order - Nothing to do
+            return
+            # Check if an order has been completed
+            # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(f'BUY EXECUTED, {order.executed.price:.2f}')
+            elif order.issell():
+                self.log(f'SELL EXECUTED, {order.executed.price:.2f}')
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin,
+                              order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # Reset orders
+        self.order = None
+
+    def log(self, txt, dt=None):  # Helper log function
+        dt = dt or self.datas[0].datetime.date(0)
+        #  print(f'{dt.isoformat()} {txt}')
+
+    def notify_trade(self, trade):  # Helper log function
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
+    def stop(self):
+        print('==================================================')
+        print('Starting Value: %.2f' % self.broker.startingcash)
+        print('Ending   Value: %.2f' % self.broker.getvalue())
+        print('Profit/Loss: %.2f' % (self.broker.getvalue() - self.broker.startingcash))
+        print('==================================================')
+
+        # Helpser Class for adding a CSV with sentiment scores
+
+
+class SentimentCSV(bt.feeds.GenericCSVData):
+    # Add a 'pe' line to the inherited ones from the base class
+    lines = ('sentiment_score',)
+
+    # openinterest in GenericCSVData has index 7 ... add 1
+    # add the parameter to the parameters inherited from the base class
+    params = (('sentiment_score', 8),)
+
 
 if __name__ == '__main__':
-
-    ticker = 'AMZN'
-    is_live = False
-
     cerebro = bt.Cerebro()
     cerebro.broker.setcash(100000)
 
     store = alpaca_backtrader_api.AlpacaStore(
         key_id=API_KEY,
         secret_key=API_SECRET_KEY,
-        paper=True
+        paper=is_paper
     )
-    if is_live:
-        broker = store.getbroker()  # or just alpaca_backtrader_api.AlpacaBroker()
-        cerebro.setbroker(broker)
-    else:
+    DataFactory = store.getdata
+    if is_backtesting:
         cerebro.broker.setcash(100000)
         cerebro.broker.setcommission(commission=0.0)
         cerebro.addsizer(bt.sizers.PercentSizer, percents=20)
+        data_timeframe = bt.TimeFrame.Minutes
+        data_compression = 60
 
-    DataFactory = store.getdata
-
-    if is_live:
+        # Add Data
         data0 = DataFactory(
             dataname=ticker,
-            timeframe=bt.TimeFrame.Days,
-        )
-    else:
-        data0 = DataFactory(
-            dataname=ticker,
-            timeframe=bt.TimeFrame.Days,
-            fromdate=pd.Timestamp('2017-12-1'),
-            todate=pd.Timestamp('2020-12-1'),
+            timeframe=data_timeframe,
+            compression=data_compression,
+            fromdate=datetime(2017, 12, 1),
+            todate=datetime(2020, 12, 1),
             historical=True)
-    cerebro.adddata(data0)
 
-    news_sentiment_data = "../../data/files/news_with_scores.csv"
-    cerebro.adddata(bt.feeds.GenericCSVData(
-        dataname=news_sentiment_data,
-        fromdate=datetime(2017, 12, 1),
-        todate=datetime(2020, 12, 1),
-        nullvalue=0.0,
-        dtformat=('%d/%m/%Y %H:%M'),
-        datetime=0,
-        time=-1,
-        high=-1,
-        low=-1,
-        open=-1,
-        close=1,
-        volume=-1,
-        openinterest=-1,
-        timeframe=bt.TimeFrame.Days))
-    """
-    tweet_sentiment_data = "../../data/files/twitter-news_with_scores.csv"
-    cerebro.adddata(bt.feeds.GenericCSVData(
-        dataname=tweet_sentiment_data,
-        fromdate=datetime(2017, 12, 1),
-        todate=datetime(2020, 12, 1),
-        nullvalue=0.0,
-        dtformat=('%Y-%m-%d %H:%M:%S'),
-        datetime=0,
-        time=-1,
-        high=-1,
-        low=-1,
-        open=-1,
-        close=1,
-        volume=-1,
-        openinterest=-1,
-        timeframe=bt.TimeFrame.Days))
-    """
+        news_sentiment_data = "../../data/files/AMZN/amzn_news_with_scores.csv"
+        data1 = SentimentCSV(
+            dataname=news_sentiment_data,
+            fromdate=datetime(2017, 12, 1),
+            todate=datetime(2020, 12, 1),
+            nullvalue=0.0,
+            dtformat=('%d/%m/%Y %H:%M'),
+            datetime=0,
+            time=-1,
+            high=-1,
+            low=-1,
+            open=-1,
+            close=-1,
+            volume=-1,
+            openinterest=-1,
+            sentiment_score=1,
+            timeframe=data_timeframe,
+            compression=data_compression)
+        """
+        tweet_sentiment_data = "../../data/files/twitter-news_with_scores.csv"
+        cerebro.adddata(SentimentCSV(
+            dataname=tweet_sentiment_data,
+            fromdate=datetime(2017, 12, 1),
+            todate=datetime(2020, 12, 1),
+            nullvalue=0.0,
+            dtformat=('%d/%m/%Y %H:%M'),
+            datetime=0,
+            time=-1,
+            high=-1,
+            low=-1,
+            open=-1,
+            close=-1,
+            volume=-1,
+            openinterest=-1,
+            sentiment_score=1,
+            timeframe=data_timeframe,
+            compression=data_compression))
+        """
+    else:  # live/paper trading
+        broker = store.getbroker()  # or just alpaca_backtrader_api.AlpacaBroker()
+        cerebro.setbroker(broker)
+        data_timeframe = bt.TimeFrame.Minutes
+        data_compression = 60
+        data_timezone = pytz.timezone('US/Eastern')
+        # Add data
+        data0 = DataFactory(
+            dataname=ticker,
+            timeframe=data_timeframe,
+            compression=data_compression
+        )
+
+        data1 = None
+        if args.news_data is not None:
+            datakwargs_news = dict(
+                timeframe=data_timeframe,
+                compression=data_compression,
+                historical=False,
+                tz=data_timezone
+            )
+            # eventuell als pandas DF einlesen? https://github.com/mementum/backtrader/blob/0426c777b0abdfafbb0988f5c31347553256a2de/backtrader/feeds/pandafeed.py
+            # https://github.com/mementum/backtrader/blob/master/samples/data-pandas/data-pandas.py
+
+    cerebro.adddata(data0)
+    cerebro.adddata(data1)
 
     """ For running single Test """
     cerebro.addstrategy(SentimentStrategy)
     cerebro.run()
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
     cerebro.plot()
-    """
-
+    
+    
+    """ # Param optimization
     cerebro.addsizer(bt.sizers.PercentSizer, percents=10)
     cerebro.addanalyzer(btanalyzers.SharpeRatio, _name="sharpe")
     cerebro.addanalyzer(btanalyzers.DrawDown, _name="drawdown")
@@ -105,4 +227,4 @@ if __name__ == '__main__':
     par_df = pd.DataFrame(par_list, columns=['low_bound', 'high_bound', 'return', 'dd', 'sharpe'])
     par_df.sort_values(by=['sharpe'], inplace=True, ascending=False)
     print(par_df)
-    """
+"""
